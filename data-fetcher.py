b42fe2cc1d8e75c53679116ce09f6a3136055c24
bogong <michael.zbg@gmail.com>
2016-11-03 00:08:35 +0800

    test fetch one stock.

--- data-fetcher.py
+++ data-fetcher.py
@@ -8,12 +8,13 @@ import sys;
 import urllib2;
 
 # The base class
-class DataFetcher:
+class DataFetcher(object):
   def __init__(self, directory):
-    self.__directory = directory
+    self._directory = directory
 
   # Fetch data of a given stock code from sources
   def Fetch(self, stock_code):
+    pass
 
 
 # The base of Netease data fetcher
@@ -31,46 +32,46 @@ class NeteaseFetcher(DataFetcher):
   ]
 
   def __init__(self, directory):
-    super(self, directory)
+    super(NeteaseFetcher, self).__init__(directory)
 
   def Fetch(self, stock_code):
     # setup the sources of a certain stock
-    data_sources = _SetupDataSources(stock_code)
+    data_sources = self._SetupDataSources(stock_code)
     # fetch the raw data
-    _FetchFromSources(stock_code, data_sources)
+    self._FetchFromSources(stock_code, data_sources)
     # process and calculate some derived data
-    _RefineData()
+    self._RefineData()
 
-  def _FetchFromSources(stock_code, data_sources):
-    for page_name in _data_pages:
+  def _FetchFromSources(self, stock_code, data_sources):
+    for page_name in self._data_pages:
       page_url = data_sources.get(page_name)
       assert page_url
-      _FetchUrl(stock_code, page_name, page_url)
+      self._FetchUrl(stock_code, page_name, page_url)
 
-  def _FetchUrl(stock_code, page_name, page_url):
+  def _FetchUrl(self, stock_code, page_name, page_url):
     print 'Fetching', page_name, 'for', stock_code, 'at', page_url
     response = urllib2.urlopen(page_url)
     content = response.read()
 
     filename = '%s.%s.csv' % (stock_code, page_name)
     print 'Saving', page_name, 'to', filename
-    f = open(os.path.join(self.__directory, filename), 'w')
+    f = open(os.path.join(self._directory, filename), 'w')
     f.write(content)
     f.close()
 
-  def _SetupDataSources(stock_code):
-    return _data_pages
+  def _SetupDataSources(self, stock_code):
+    return {}
 
-  def _RefineData():
+  def _RefineData(self):
     pass
 
 
 # Netease per season data fetcher
 class NeteaseSeasonFetcher(NeteaseFetcher):
   def __init__(self, directory):
-    super(self, directory)
+    super(NeteaseSeasonFetcher, self).__init__(directory)
 
-  def _SetupDataSources(stock_code):
+  def _SetupDataSources(self, stock_code):
     return {
         'balance': ('http://quotes.money.163.com/service/zcfzb_%s.html' % stock_code),
         'income': ('http://quotes.money.163.com/service/lrb_%s.html' % stock_code),
@@ -85,27 +86,11 @@ class NeteaseSeasonFetcher(NeteaseFetcher):
 
 
 def main():
-  assert len(sys.argv) > 1, 'Company list is required'
-  print 'Reading company list from ', sys.argv[1]
-  reader = csv.reader(open(sys.argv[1], 'r'))
-
-  success = 0
-  total = 0
-  for num, code, name in reader:
-    total += 1
-    # extract stock code
-    m = re.match(r'^\w\w(\d+)$', code)
-    if m is None:
-      print 'Stock code is invalid:', code
-      continue
-    code = m.group(1)
-    # decode stock name
-    name = name.decode('GBK').encode('utf-8')
-    FetchData(code, name)
-    success += 1
-
-  print 'Total:', total, 'success:', success
-
+  directory = './data/test'
+  stock_code = '000977'
+  fetcher = NeteaseSeasonFetcher(directory)
+  print 'Fetching', stock_code
+  fetcher.Fetch(stock_code)
 
 if __name__ == "__main__":
   main()
